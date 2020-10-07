59ed7ef7d1352d0e525f3c7dadec2f229fdd7428
Giorgi <giorgi@giorgi.dev>
2020-10-07 19:23:09 +0400

    Trying to get user info

--- OktaDemo/Controllers/HomeController.cs
+++ OktaDemo/Controllers/HomeController.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Okta.Sdk;
+using Okta.Sdk.Configuration;
 using OktaDemo.Models;
 
 namespace OktaDemo.Controllers
@@ -15,17 +18,28 @@ namespace OktaDemo.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            this.configuration = configuration;
         }
 
         public async Task<IActionResult> Index()
         {
             var userClaims = HttpContext.User.Claims;
+            var email = userClaims.FirstOrDefault(claim => claim.Type=="email").Value;
 
             var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            var client = new OktaClient(new OktaClientConfiguration
+            {
+                OktaDomain = configuration.GetSection("Okta").GetValue<string>("OktaDomain"),
+                Token = accessToken
+            });
+
+            var user = await client.Users.GetUserAsync(email);
 
             return View();
         }
